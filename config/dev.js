2e4ad57b7f239fd6bb43ea2eb12aefc950fc3c18
Ahmed Tahir Shekhani - 10PEARLS <108052439+ahmedtahirshekhani-10P@users.noreply.github.com>
2022-07-05 11:55:37 +0500

    MongoDB URI updated in dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DB_URI: "mongodb://localhost:27017/travelitinerary-app",
+    DB_URI: "process.env.MONGODB_URI",
     // SECRET: process.env.SECRET,
     // AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     // AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
