dc310a47c2dec584cefde16c4810acda9439f5aa
Ahmed Tahir Shekhani <ahmed.tahir@10pearls.com>
2022-07-07 13:40:53 +0500

    config added

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DB_URI: process.env.MONGODB_URI,
+    SECRET: "asda2$3w3213askhlkjkldalkl1o8@#",
+    // AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    // AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    // STRIPE_SK: process.env.STRIPE_SK
+  }
+  
+  
+  
